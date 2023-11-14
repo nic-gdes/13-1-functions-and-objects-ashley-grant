0952292296ae530196e9f787903cb73f18d77563
Ashley Grant <agrant2@nic.edu>
2023-11-14 15:48:00 -0800

    step 1 and 2

--- script.js
+++ script.js
@@ -1,2 +1,72 @@
 // Nothing here yet...
 // Make sure to link your JS in index.html!
+//start with sudo code
+
+
+
+// const obj = {
+//     prop: "Property1",
+//     newMethod() {
+//         return this.prop;
+//     },
+//     newArray: [1,2,3,4],
+//     newObj: {
+//         property1: "property1"
+//     },
+//     "Key": "Value",
+// }
+
+// const newFunction = function() {
+
+// }
+
+// console.log(obj.newObj.property1());
+
+
+
+
+
+// Create a Car class, with a constructor that accepts 3 parameters: make (ex: Chevy, Ford, Toyota), model (ex: Camero, F150, Rav4), and year.
+class Car {
+    constructor(make, model, year) {
+    this.make = "Chevy";
+    this.model = "Malibu";
+    this.year = "2024";
+    }
+
+
+// Car should also include a method, .describe which simply returns "This is a YEAR MODEL, made by MAKE." replacing YEAR, MODEL and MAKE with the correct information from the Car object.
+
+describe() {
+    return("This is a " + this.year + ",made by " + this.make);
+}
+}
+const chevyMalibu = new Car("Chevrolet", "Malibu", "2024");
+
+chevyMalibu.describe
+
+
+ // This is a 2024 Malibu, made by Chevrolet.
+
+
+// Create 4 cars (with any make, model and year) and push them all to a new array called allCars.
+
+    // Create a new array called allCars.
+
+    // You will need to create 4 new variables, using the const {{carname}} = new Car() syntax. {{ carname }} is just whatever variable name you'll choose.
+
+
+// Write a function with a single parameter car that, when called, will console.log the Car.describe method for the car used in the parameter.
+
+    // Declare the function using function {{ functionName }} (car) {};
+
+    // Inside the function's code block, simply return car.describe; so the function returns the describe phrase for the car in the parameters.
+
+
+// Create an array called allCars, that includes all of the created cars. You can use array.push() or simply create the array with the data.
+
+
+// Loop through allCars and call the function to log each car's describe method to the console one by one.
+    // You can use a for(), or forEach loop to achieve this. Remember to use allCars.length in your end condition if you use a for() loop.
+    // Remember you can call a function by simply writing the function with parenthesis around the parameters:
+        // functionName(param1, param2, param3, ...);
